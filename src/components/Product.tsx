babdc96e8f999609209afa5d66731e3bd5a58503
dmytro <dimitritkach@gmail.com>
2022-10-08 19:11:01 +0200

    useState, dinamic classes, inline style

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,20 +1,48 @@
 
 import { IProduct } from '../models';
+import { useState } from 'react';
 interface ProductProps {
   product: IProduct
 }
 
 export function Product({ product }: ProductProps) {
+
+  const [details, setDetails] = useState(false)
+
+  const btnBgClassname = details ? 'bg-slate-100' : 'bg-slate-300'
+
+  const btnClasses = [
+    'px-4 py-2 border',
+    btnBgClassname
+  ]
+
   return (
     <div
       className="border py-2 px-4 rounded flex flex-col items-center mb-2"
     >
+
       <img src={product.image} alt={product.title} className="w-1/6" />
+
       <p>
         {product.title}
       </p>
+
       <p className='font-bold'>{product.price}</p>
-      <p>{product.description}</p>
+
+      <button
+        onClick={() => setDetails(prev => !prev)} 
+        className={btnClasses.join(' ')}
+        >
+        {details ? 'Hide Details':'Show Details'}
+      </button>
+
+      {details && <div>
+        <p>{product.description}</p>
+        <p>Rate: <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span></p>
+      </div> 
+
+      }
+
     </div>
   )
 }
