0b418221eb83796f93600681b53a8b45618dbfa2
dmytro <dimitritkach@gmail.com>
2022-10-10 12:36:46 +0200

    Loader, ErrorMessage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 
+import { ErrorMessage } from "./components/ErrorMessage";
+import { Loader } from "./components/Loader";
 import { Product } from "./components/Product";
 import { useProducts } from './hooks/products';
 
@@ -8,8 +10,8 @@ function App() {
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <p className='text-center'>Loading...</p>}
-      {error && <p className='text-center text-red-500'>{error}</p>}
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
       { products.map(product => <Product key={product.id} product={product} />) }
     </div>
   )
