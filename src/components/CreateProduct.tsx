4d1dd5a7fc979b7633ecbda5ec26f9b0b1c67c36
dmytro <dimitritkach@gmail.com>
2022-10-10 14:02:32 +0200

    create product on form submit with custom title

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,11 +1,37 @@
 import { FormEvent, useState, ChangeEvent } from "react"
+import { IProduct } from '../models';
+import axios from "axios";
+import { ErrorMessage } from './ErrorMessage';
+
+const productData: IProduct =  {
+  title: '',
+  price: 13.5,
+  description: 'lorem ipsum set',
+  image: 'https://i.pravatar.cc',
+  category: 'electronic',
+  rating: {
+    rate: 4.5,
+    count: 10
+  }
+}
 
 export function CreateProduct() {
 
   const [value, setValue] = useState('')
 
-  const submitHandler = (event: FormEvent) => {
+  const [error, setError] = useState('')
+
+  const submitHandler = async (event: FormEvent) => {
     event.preventDefault()
+    setError('')
+
+    if (value.trim().length === 0) {
+      setError('Please enter valid Title!')
+      return
+    }
+
+    productData.title = value
+    const response =  await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
   }
 
   const cangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
@@ -22,6 +48,9 @@ export function CreateProduct() {
         value={value}
         onChange={cangeHandler}
       />
+
+      { error && <ErrorMessage error={error} /> }
+
       <button type="submit" 
         className="border px-4 py-2 bg-slate-300 hover:bg-slate-400 hover:text-gray-200"
         >
