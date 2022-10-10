37689349b6142b32c3665b01eef3edc9c5602da0
dmytro <dimitritkach@gmail.com>
2022-10-10 12:59:04 +0200

    React.FormEvent type

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,6 +1,15 @@
+import { FormEvent } from "react"
+
 export function CreateProduct() {
+
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault()
+  }
+
   return (
-    <form>
+    <form 
+      onSubmit={submitHandler}
+      >
       <input type="text"
         className="border px-4 py-2 mb-2 w-full outline-0"
         placeholder="Enter Product Title!" 
