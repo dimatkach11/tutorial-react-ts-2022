9dd0d305266c35993924b6d76cfb2a3c1ff8c434
dmytro <dimitritkach@gmail.com>
2022-10-10 13:48:09 +0200

    ChangeEvent<HTMLInputElement>

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,9 +1,15 @@
-import { FormEvent } from "react"
+import { FormEvent, useState, ChangeEvent } from "react"
 
 export function CreateProduct() {
 
+  const [value, setValue] = useState('')
+
   const submitHandler = (event: FormEvent) => {
     event.preventDefault()
+  }
+
+  const cangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
   }
 
   return (
@@ -12,7 +18,9 @@ export function CreateProduct() {
       >
       <input type="text"
         className="border px-4 py-2 mb-2 w-full outline-0"
-        placeholder="Enter Product Title!" 
+        placeholder="Enter Product Title!"
+        value={value}
+        onChange={cangeHandler}
       />
       <button type="submit" 
         className="border px-4 py-2 bg-slate-300 hover:bg-slate-400 hover:text-gray-200"
