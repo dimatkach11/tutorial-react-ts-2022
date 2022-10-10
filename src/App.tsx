1850536748f649f2c186d76dac352367f4fd1734
dmytro <dimitritkach@gmail.com>
2022-10-10 09:42:53 +0200

    loading when fetching products

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,22 @@ function App() {
 
   const [products, setProducts] = useState<IProduct[]>([])
 
+  const [loading, setLoading] = useState(false)
+
   async function fetchProducts() {
+    setLoading(true)
     const response =  await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
     setProducts(response.data)
+    setLoading(false)
   }
 
   useEffect(() => {
     fetchProducts()
   }, [])
-  
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <p className='text-center'>Loading...</p>}
       { products.map(product => <Product key={product.id} product={product} />) }
     </div>
   )
